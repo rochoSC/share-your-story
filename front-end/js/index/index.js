32fd7728591c4ae024340539824fa67d3f66f3b3
Ivan <jivfur@gmail.com>
2018-03-29 21:46:00 +0000

    index

--- front-end/js/index/index.js
+++ front-end/js/index/index.js
@@ -1,142 +1,119 @@
 
-
-function rightControl(content){
-  //  $(control).mouseover(function() {
-      event.preventDefault();
-      //if(parseInt($(content).css('marginRight'))<0){
-        $(content).animate({
-          marginLeft: "-=400px"
-        }, "fast");
-      //}
-   //});
-}
-
-function leftControl(content){
- //$('#controlL').mouseover(function() {
-      event.preventDefault();
-      if(parseInt($(content).css('marginLeft'))<0){
-       $(content).animate({
-          marginLeft: "+=400px"
-        }, "fast");
-    }
- //});
-
-}
-
 $(document).ready(function() {
   
   
-    
+
 
   var usernameSession = localStorage.getItem("usernameSession");
 
-  if(usernameSession){
+  if (usernameSession) {
     $("#usernameSession").show();
     $("#goToDashboard").show();
     $("#shareStory").hide();
-  }else{
+  }
+  else {
     $("#usernameSession").hide();
     $("#goToDashboard").hide();
     $("#shareStory").show();
   }
-  
+
   //This file must be added on every js that uses ajax calls to our API server
-  $.getScript("js/constants.js", function() {
-     console.log("Constants file loaded")
-     //Load the list videos
-    var videos = $.ajax({type: "GET",
+  $.getScript("js/constants.js", function() {console.log("Constants file loaded")
+    //Load the list videos
+   $.ajax({
+      type: "GET",
       contentType: "application/json",
       url: CONSTANTS.API_BASE_URL + "videos",
       data: JSON.stringify({}),
       dataType: 'json',
-      success: function(msg){
+      success: function(msg) {
         //fill the categories
-        console.log(msg);
-        
-        
-        jQuery.each(msg, function(index1,categories){
-          var $carrusel = $("<div>");
-          var $newCat = $("<div class='title text-left h1'>".concat(index1).concat("</div>") )
-          var $idControl = "'#content".concat(index1).concat("'");
-          var $controles=$("<div id='controlL' class='left-controls' role='button' aria-label='See Previous Modules' onmouseover='leftControl(".concat($idControl).concat(")' ><b class='fa fa-chevron-left fa-chevron-left-extra' aria-hidden='true' ></b></div>"));
-          var $listaVideos = $("<ul id=".concat($idControl).concat(">"));
-          $carrusel.append($newCat)
-          $carrusel.append($controles)
-          // $("#ul").append($("<li>").text("Some Text."));
-          jQuery.each(categories,function(index2,video){
-           var $thumbnail = $("<img>").attr("src",video["thumbnail"]).attr("height","150px").attr("width","150px");
-           var $title = $("<p>").addClass("text-light bg-dark").text(video["title"]);
-           var $inside = $("<div>").addClass("inside-top");
-            $inside.append($thumbnail)
-            $inside.append($title)
-            var $item = $("<li>").addClass("card effect1");
-            $item.append($thumbnail)
-            $item.append($title)
-            $listaVideos .append($item)
-            $carrusel.append($listaVideos)
-          });
-          var $arrowR = $("<b class='fa fa-chevron-right fa-chevron-right-extra' aria-hidden='true' ></b>");
-          var $controlR = $("<div>").attr("id","controlR").addClass("right-controls").attr("role","button").attr("aria-label","See Previous Modules").attr("onmouseover","rightControl(".concat($idControl).concat(")"));
-          $controlR.append($arrowR);
-          $carrusel.append($controlR)
-          $("#wrapper").append($carrusel);
-        });
-        
-        
-        // 
-        
-    
-    
-        // 
-        // $("#wrapper").append($controles)
+        jQuery.each(msg, function(index1, categories) {
+          
+          var $idControl = "#content".concat(index1)
+          //console.log($idControl)
+          var $category = $("<div>") //creating the slide for category
+          var $categoryTitle = $("<div class='title text-left h1'>".concat(index1).concat("</div>"));
+          $category.append($categoryTitle);
+          var $myCarrusel = $("<div>").attr("id",$idControl).addClass("carousel slide");
+          var $innercarousel = $("<div>").addClass("carousel-inner");
+          var $classItem = "item active";
+          for( v=0;v<categories.length;){
+            var $item = $("<div>").addClass($classItem);
+            var $classItem = "item";
+            //hay un for aqui que es modulo 4
+            var $row =$("<div>").addClass("row");
+            //hay un for que va hasta el 4
+            for(i=0; i<4&&i<categories.length ;i++ ){
+              var $video = $("<div>").addClass("col-xs-3");
+              var $link = $("<a>").attr("href","#?id=".concat(categories[v]["id"])).addClass("thumbnail").attr("data-target","#VideoWatch").attr("data-toggle","modal").data("src",categories[v]["url"]);
+              $link.data("title",categories[v]["title"]).data("description",categories[v]["description"])
+              var $thumbnail = $("<img>").attr("src",categories[v]["thumbnail"]); //add thumbnailroute
+              $link.append($thumbnail);
+              $video.append($link);
+              $row.append($video);
+              v++;
+            }//termina el for que va hasta el 4
+            //termina el for 
+            $item.append($row)
+          }
+          var $leftControl = $("<a>").addClass("left carousel-control").attr("href",$idControl).attr("data-slide","prev").text("‹")
+          var $rightControl = $("<a>").addClass("right carousel-control").attr("href",$idControl).attr("data-slide","next").text("›")
+          $innercarousel.append($item);
+          $myCarrusel.append($innercarousel);
+          $myCarrusel.append($leftControl);
+          $myCarrusel.append($rightControl);
+          $category.append($myCarrusel);
+          console.log($category[0])
+          $("#wrapper").append($category);
           
           
+        }); //every category first for
         
       },
-      error: function(msg){
+      error: function(msg) {
         //something happened
         //maybe show an alert of something like that.
-        alert("Something happened")
-        
+        alert("Something happened");
       }
-      
-      });
+    });
+    
   });
 
   //Submit loggin info
-  $("#loginForm").submit(function(){
+  $("#loginForm").submit(function() {
 
     $.ajax({
-    type: "POST",
-    contentType: "application/json",
-    url: CONSTANTS.API_BASE_URL + "user/login" ,
-    data: JSON.stringify({
-      username: $("#loginForm #usernameL").val(),
-      password: $("#loginForm #passwordL").val()
-    }),
-    dataType: 'json',
-    success: function(msg){
-      $("#loginForm #loginErrorMsg").hide();
-      //Storing on localStorage for future session references
-      localStorage.setItem("usernameSession",$("#loginForm #usernameL").val().toLowerCase());
-      window.location = "dashboard"
-    },
-    error: function(msg){
-      $("#loginForm #loginErrorMsg").text(msg.responseJSON.message);
-      $("#loginForm #loginErrorMsg").fadeIn();
-    }
+      type: "POST",
+      contentType: "application/json",
+      url: CONSTANTS.API_BASE_URL + "user/login",
+      data: JSON.stringify({
+        username: $("#loginForm #usernameL").val(),
+        password: $("#loginForm #passwordL").val()
+      }),
+      dataType: 'json',
+      success: function(msg) {
+        $("#loginForm #loginErrorMsg").hide();
+        //Storing on localStorage for future session references
+        localStorage.setItem("usernameSession", $("#loginForm #usernameL").val().toLowerCase());
+        window.location = "dashboard"
+      },
+      error: function(msg) {
+        $("#loginForm #loginErrorMsg").text(msg.responseJSON.message);
+        $("#loginForm #loginErrorMsg").fadeIn();
+      }
     });
     return false;
   });
 
-  $("#registerForm").submit(function(){
+  $("#registerForm").submit(function() {
 
-    if ($("#passwordR").val().length < 6){
+    if ($("#passwordR").val().length < 6) {
       $("#registerForm #registerErrorMsg").text("Password too short");
       $("#registerForm #registerErrorMsg").fadeIn();
       return false;
     }
-    if ($("#passwordR").val() !== $("#confirmPasswordR").val()){
+    if ($("#passwordR").val() !== $("#confirmPasswordR").val()) {
       $("#registerForm #registerErrorMsg").text("Passwords does not match");
       $("#registerForm #registerErrorMsg").fadeIn();
       return false;
@@ -144,41 +121,57 @@ $(document).ready(function() {
     $("#registerForm #registerErrorMsg").hide();
 
     $.ajax({
-    type: "POST",
-    contentType: "application/json",
-    url: CONSTANTS.API_BASE_URL + "user/register" ,
-    data: JSON.stringify({
-      username: $("#registerForm #usernameR").val().toLowerCase(),
-      password: $("#registerForm #passwordR").val(),
-      name: $("#registerForm #nameR").val(),
-      lastname: $("#registerForm #lastnameR").val()
-    }),
-    dataType: 'json',
-    success: function(msg){
-      $("#registerForm #registerErrorMsg").hide();
-      //Storing on localStorage for future session references
-      localStorage.setItem("usernameSession",$("#registerForm #usernameR").val().toLowerCase());
-      window.location = "dashboard"
-    },
-    error: function(msg){
-      $("#registerForm #registerErrorMsg").text(msg.responseJSON.message);
-      $("#registerForm #registerErrorMsg").fadeIn();
-    }
+      type: "POST",
+      contentType: "application/json",
+      url: CONSTANTS.API_BASE_URL + "user/register",
+      data: JSON.stringify({
+        username: $("#registerForm #usernameR").val().toLowerCase(),
+        password: $("#registerForm #passwordR").val(),
+        name: $("#registerForm #nameR").val(),
+        lastname: $("#registerForm #lastnameR").val()
+      }),
+      dataType: 'json',
+      success: function(msg) {
+        $("#registerForm #registerErrorMsg").hide();
+        //Storing on localStorage for future session references
+        localStorage.setItem("usernameSession", $("#registerForm #usernameR").val().toLowerCase());
+        window.location = "dashboard"
+      },
+      error: function(msg) {
+        $("#registerForm #registerErrorMsg").text(msg.responseJSON.message);
+        $("#registerForm #registerErrorMsg").fadeIn();
+      }
     });
     return false;
   });
 
   //Showing register view when clicked on create account
-  $("#createAccount").click(function(){
+  $("#createAccount").click(function() {
     $("#loginView").hide();
     $("#registerView").show();
   });
 
   //Showing login view when clicked on back to login button
-  $("#backToLogin").click(function(){
+  $("#backToLogin").click(function() {
     $("#registerView").hide();
     $("#loginView").show();
   });
+  
+  $(document).on("click", ".thumbnail", function () {
+     var myVideoURL = $(this).data('src');
+     if (!myVideoURL.includes("embed")){
+       pos =myVideoURL.lastIndexOf("/")
+       myVideoURL = "http://www.youtube.com/embed/".concat(myVideoURL.substr(pos+1,myVideoURL.length))
+     }
+     $(".modal-body #videoToWatch").attr( "src", myVideoURL);
+     $("#videoTitle").text($(this).data('title'))
+     $("#titleDescription").text($(this).data('description'))
+     
+     // As pointed out in comments, 
+     // it is superfluous to have to manually call the modal.
+     // $('#addBookDialog').modal('show');
+});
+  
 });
 /*jQuery(document).ready(function ($) {
 
